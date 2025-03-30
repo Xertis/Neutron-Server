3ffe92b552e549f649a2aea9de1e2c7c05a27d87
Xertis <118364459+Xertis@users.noreply.github.com>
2025-03-30 15:14:10 +0300

    Update container.lua

--- modules/lib/private/common/container.lua
+++ modules/lib/private/common/container.lua
@@ -13,6 +13,7 @@ local DATA = {
 }
 
 function module.accounts.put(username, account)
+    print(debug.traceback())
     print("PUT", username, json.tostring(account))
     DATA.accounts[username] = account
 end
