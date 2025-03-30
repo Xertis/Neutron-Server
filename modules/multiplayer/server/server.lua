8abbd90e11ee0ec8d30868620dc267495ec727a9
Xertis <118364459+Xertis@users.noreply.github.com>
2025-03-30 14:15:18 +0300

    упс, а это мы уберём

--- modules/multiplayer/server/server.lua
+++ modules/multiplayer/server/server.lua
@@ -32,7 +32,7 @@ function server:start()
         local client = Player.new(false, network, address, port)
 
         for i, mclient in ipairs(self.clients) do
-            if mclient.address == client.address and not self.clients[i].active then
+            if mclient.address == client.address then
                 self.clients[i].network = client.network
                 self.clients[i].port = client.port
                 return
