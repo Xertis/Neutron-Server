dca6db49692acdaa207ce9b0e28b5e4ac628decf
Xertis <118364459+Xertis@users.noreply.github.com>
2025-05-23 19:15:40 +0300

    доки + патч движка

--- modules/init/engine_patcher.lua
+++ modules/init/engine_patcher.lua
@@ -6,3 +6,32 @@ entities["despawn"] = function (eid)
     entities_despawn(eid)
     entities_manager.despawn(eid)
 end
+
+--- Патчим корутины
+
+local __vc_resume_coroutine_default = __vc_resume_coroutine
+local __vc_coroutines = nil
+
+for i = 1, math.huge do
+    local name, value = debug.getupvalue(__vc_resume_coroutine_default, i)
+    if not name then break end
+
+    if name == "__vc_coroutines" then
+        __vc_coroutines = value
+    end
+end
+
+if __vc_coroutines ~= nil then
+    __vc_resume_coroutine = function(id)
+        local co = __vc_coroutines[id]
+        if not co then return false end
+
+        local success, err = pcall(coroutine.resume, co)
+        if not success then
+            debug.error(err)
+            logger.log("Engine coroutine error: " .. tostring(err), 'P')
+        end
+
+        return coroutine.status(co) ~= "dead"
+    end
+end
