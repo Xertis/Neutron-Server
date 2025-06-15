a9d7ccf6604758363788eb6e075f9357afcd888d
Xertis <118364459+Xertis@users.noreply.github.com>
2025-06-15 12:55:28 +0300

    чучут улучшил

--- modules/api/entities.lua
+++ modules/api/entities.lua
@@ -1,9 +1,25 @@
 local entities_manager = start_require "lib/private/entities/entities_manager"
 
-local module = {}
+local HUGE = math.huge
+
+local module = {
+    eval = {}
+}
 
 function module.register(entity_name, config, handler)
     entities_manager.register(entity_name, config, handler)
+end
+
+function module.eval.NotEquals(dist, cur_val, client_val)
+    return cur_val ~= client_val and HUGE or 0
+end
+
+function module.eval.Always()
+    return HUGE
+end
+
+function module.eval.Never()
+    return 0
 end
 
 return module
