b56db7aab961ac282da6d711f8eb0ec4f9f6b6f8
Xertis <118364459+Xertis@users.noreply.github.com>
2025-06-25 21:44:10 +0300

    Update account_manager.lua

--- modules/lib/private/accounts/account_manager.lua
+++ modules/lib/private/accounts/account_manager.lua
@@ -70,7 +70,7 @@ function module.get_client(account)
 
     for _, client in pairs(container.clients_all.get()) do
         if not client.account then
-            logger.log("Account information lost. Client: " .. json.tostring(client), "E")
+            logger.log("Account information lost.", "E")
             goto continue
         end
         if client.account.username == account.username then
